3735d5c2ab66603a04271f6df82460a39eb23bd5
Alamirew <akliluarse@gmail.com>
2025-05-16 17:42:15 +0300

    ff

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,18 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+   base: '/MyPortfolio/',
+    plugins: [react()],
+    build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          framer: ['framer-motion'],
+          lottie: ['lottie-web', '@lottiefiles/react-lottie-player'],
+        }
+      }
+    }
+  }
 })
+
+
